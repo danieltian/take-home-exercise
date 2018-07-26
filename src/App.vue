b08473507af9cc31bc1e01c4d615702836a777bb
Daniel Tian <dftian@gmail.com>
2018-07-25 15:21:00 -1000

    Initial Webpack+Vue scaffolding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  #app
+    h1 {{ text }}
+    input(v-model="text")
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        text: 'Hi from app!'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  h1
+    color: orange
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const { HotModuleReplacementPlugin } = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    })
+  ],
+
+  devServer: {
+    hotOnly: true, // Enable hot module replacement with no fallback to page refresh.
+    overlay: true, // Show an overlay of any build errors in the browser.
+    open: true, // Automatically open the browser when dev server is started.
+    contentBase: path.resolve(__dirname, 'dist'), // Use dist folder as the root of the web server.
+    stats: 'errors-only' // Only show that a compile is happening, but hide the bundle info to avoid its spam.
+  },
+
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        use: 'eslint-loader'
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-plain-loader'
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: ['vue-style-loader', 'css-loader', 'stylus-loader']
+      }
+    ]
+  }
+}
